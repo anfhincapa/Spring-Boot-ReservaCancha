66d4cd3fb157487252aa2e3ab6586375e97284c8
andres <andresmouse_35@hotmail.com>
2021-01-25 16:40:19 -0500

    comit inicial con eclipse

---
+++ CanchaSintetica/src/main/java/com/andresportsco/cancha/CanchaSinteticaApplication.java
@@ -0,0 +1,13 @@
+package com.andresportsco.cancha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanchaSinteticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanchaSinteticaApplication.class, args);
+	}
+
+}
