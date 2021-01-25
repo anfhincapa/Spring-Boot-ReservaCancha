1255640df1e5b3726d10f3de3f5670c0ea28cdc0
andres <andresmouse_35@hotmail.com>
2021-01-25 16:40:19 -0500

    comit inicial con eclipse. Close #1

---
+++ CanchaSintetica/src/test/java/com/andresportsco/cancha/CanchaSinteticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresportsco.cancha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanchaSinteticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
